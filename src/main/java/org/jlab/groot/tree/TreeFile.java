e1e558ba42692c801c1f2fdd4c7134171704eeb5
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-10-16 01:21:45 +0300

    added desktop and TreeFile

---
+++ src/main/java/org/jlab/groot/tree/TreeFile.java
@@ -0,0 +1,13 @@
+package org.jlab.groot.tree;
+
+public class TreeFile extends Tree {
+
+
+    public TreeFile(String name) {
+        super(name);
+    }
+
+    public void readFile(String file){
+
+    }
+}
