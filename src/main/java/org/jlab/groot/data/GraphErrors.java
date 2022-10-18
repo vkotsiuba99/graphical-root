5ecd7685dce13b7194234af58d00df0448a04ffb
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-10-19 01:01:48 +0300

    added function type in DatasetAttributes

--- src/main/java/org/jlab/groot/data/GraphErrors.java
+++ src/main/java/org/jlab/groot/data/GraphErrors.java
@@ -40,6 +40,7 @@ public class GraphErrors implements IDataSet {
         graphAttr.setMarkerColor(1);
         graphAttr.setMarkerSize(6);
         graphAttr.setMarkerStyle(1);
+        graphAttr.setDatasetType(DatasetAttributes.GRAPHERRORS);
 
     }
 
