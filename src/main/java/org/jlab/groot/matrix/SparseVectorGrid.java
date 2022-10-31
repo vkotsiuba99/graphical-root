c8aeb212d594426c418d0d1bc7fadad9468905e7
Vladyslav Kotsiuba <92172262+vkotsiuba99@users.noreply.github.com>
2022-10-31 15:03:31 +0200

    added IndexPane for display

--- src/main/java/org/jlab/groot/matrix/SparseVectorGrid.java
+++ src/main/java/org/jlab/groot/matrix/SparseVectorGrid.java
@@ -29,6 +29,10 @@ public class SparseVectorGrid {
 
     public HashMap<Long,DataVector> getGrid(){ return binMap;}
 
+    public int getVectorSize(){
+        return this.vectorSize;
+    }
+
 
     public void addBin(int[] index){
         Long key = indexer.getKey(index);
